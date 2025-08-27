9eff0062c92be0c291ddb8674a180487f3745f01
ACT-STUDENT <ACT-STUDENT@LAB3-PC11>
2025-08-27 14:43:14 +0800

    Program on Basic and Encapsulation

---
+++ Esquierdo_PropertiesAndEncapsulation/Esquierdo_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object of shirt
+            Shirt shirt1 = new Shirt();
+            //Set values using propeties
+            shirt1.Brand = "UNIQLO";
+            shirt1.Color = "Plane White";
+
+            //Desplay Details
+            shirt1.DisplayDetails();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+        
+    
+
---
+++ Esquierdo_PropertiesAndEncapsulation/Esquierdo_PropertiesAndEncapsulation/Shirt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_PropertiesAndEncapsulation
+{
+    internal class Shirt
+    {
+        //Declare the properties of the shirt class
+        private string brand;
+        private string color;
+
+        //Set the Properties of the shirt class
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        //Method to Dispaly Details
+        public void DisplayDetails()
+
+        {
+            Console.WriteLine("Bag Information:");
+            Console.WriteLine("Brand:" + brand);
+            Console.WriteLine("Color:" + color);
+        }
+    }
+}
+
+    
+
