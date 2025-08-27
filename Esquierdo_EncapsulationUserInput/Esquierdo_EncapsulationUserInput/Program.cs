c44efbbc4388bfee801b97432355726f5b51cefd
ACT-STUDENT <ACT-STUDENT@LAB3-PC11>
2025-08-27 14:53:29 +0800

    Program on Basic Encapsulation

---
+++ Esquierdo_EncapsulationUserInput/Esquierdo_EncapsulationUserInput/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for input
+            Console.Write("Enter Shirt Brand: ");
+            string brandInput = Console.ReadLine();
+
+            Console.Write("Enter Shirt Color: ");
+            string colorInput = Console.ReadLine();
+
+            // Instantiate Shirt object with user input
+            Shirt shirt1 = new Shirt
+            {
+                Brand = brandInput,
+                Color = colorInput
+            };
+
+            // Display Shirt details
+            shirt1.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
---
+++ Esquierdo_EncapsulationUserInput/Esquierdo_EncapsulationUserInput/Shirt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_EncapsulationUserInput
+{
+    internal class Shirt
+    {
+        // Auto-implemented properties (short-hand encapsulation)
+        public string Brand { get; set; }
+        public string Color { get; set; }
+
+        // Method to display details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("\nShirt Information:");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Color: {Color}");
+        }
+    }
+}
+
+    
+
