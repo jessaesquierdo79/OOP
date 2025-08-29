d2c6246f08a0eca834ed82f728746b2c573944f2
ACT-STUDENT <ACT-STUDENT@LAB3-PC11>
2025-08-29 14:33:39 +0800

    Program on Creating   a Class and Object

---
+++ Esquierdo_CaO/Esquierdo_CaO/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_CaO
+{
+    //Name:Jessa Mae Esquierdo
+    //Section: IT306
+    //TASK : Create a class
+     class Person
+     {
+        
+   
+            // Class Members
+            public string name;
+            public int age;
+            public string gender;
+
+
+            //Method Display 
+            public void Greet()
+            {
+                Console.WriteLine("Hi uy! My name is " + name + "and I am " + age + " years of age, and I'm a " + gender);
+            }
+
+
+
+        
+    }
+}
+
---
+++ Esquierdo_CaO/Esquierdo_CaO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_CaO
+{
+    internal class Program
+    {
+        //Name:Jessa Mae Esquierdo
+        //Section: IT306
+        //TASK : Create a class
+        static void Main(string[] args)
+        {
+            {
+                Person personal = new Person();  //Instantiation of an object 
+
+                //Instantiation of fields
+                personal.name = "Jessa MAe Esquierdo";
+                personal.age = 19;
+                personal.gender = "Female";
+
+                //Call The Method 
+                personal.Greet();
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+    
+
