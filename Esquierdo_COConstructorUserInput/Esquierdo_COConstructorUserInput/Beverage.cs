85b6a42b4226c7ac7a7310cfedac83007b31283c
ACT-STUDENT <ACT-STUDENT@LAB3-PC11>
2025-08-29 16:22:01 +0800

    Program on  Constructor User Input

---
+++ Esquierdo_COConstructorUserInput/Esquierdo_COConstructorUserInput/Beverage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_COConstructorUserInput
+{
+    internal class Beverage
+    {
+        //Attributes
+        public string brand;
+        public string size;
+        public string flavor;
+
+        public Beverage(string aBrand, string aSize, string aFlavor)  //Parameterized Constructor
+        {
+            brand = aBrand;
+            size = aSize;
+            flavor = aFlavor;
+        }
+
+        //Method to Dsiplay the Beverage Details
+        public void DisplayBeverageDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("-------- Here are the Beverage Details --------");
+            Console.WriteLine("Beverage Brand: " + brand);
+            Console.WriteLine("Beverage Size: " + size);
+            Console.WriteLine("Beverage Flavor: " + flavor);
+
+        }
+
+    }
+}
---
+++ Esquierdo_COConstructorUserInput/Esquierdo_COConstructorUserInput/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+            //Ask the user to input
+            Console.WriteLine("This program will ask about your favorite beverage. \n\n");
+            Console.Write("Enter brand name (Coke/Sprite/Royal): \t");
+            string aBrand = Console.ReadLine();
+
+            Console.Write("Enter size (8oz/12oz):      \t");
+            string aSize = Console.ReadLine();
+
+            Console.Write("Enter flavor (Ex. Coke: Regular or Zero):   \t");
+            string aFlavor = Console.ReadLine();
+
+            //Instantiate a class object
+            Beverage beverage1 = new Beverage(aBrand, aSize, aFlavor);
+
+            //Call the method to display
+            beverage1.DisplayBeverageDetails();
+
+            //Input another book details
+            Console.Write("\nDo you want to try again (y/n)? ");
+            char ans = Convert.ToChar(Console.ReadLine());
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if (ans == 'n')
+            {
+                return;
+            }
+        }
+    }
+}
+
+        
+    
+
