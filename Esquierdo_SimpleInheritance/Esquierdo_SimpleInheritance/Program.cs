b8baac9d4bd73ab679d1b9910909a597a4316cfd
ACT-STUDENT <ACT-STUDENT@LAB4-PC1>
2025-09-02 16:40:27 +0800

    Program on Simple Inheritance

---
+++ Esquierdo_SimpleInheritance/Esquierdo_SimpleInheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_SimpleInheritance
+{
+    internal class Program
+    {
+        // Name: Jessa Mae Esquierdo
+        // Section: IT306
+        // Exercise Title:Use these classes:  Student class (Base class); RegularStudent class (Derived class); IrregularStudent class (Derived class)
+
+        static void Main(string[] args)
+        {
+            //Create an instance of the Regular student
+            Regular myRegular = new Regular
+            {
+                Name = "Jessa Mae Esquierdo",
+                Program = "BSIT",
+                Section = "IT306"
+            };
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n\t*****Regular Student****\n");
+            myRegular.StudentBasicInfo();
+            myRegular.SectionEnrolled();
+
+            //Create an instance of the Irregular class
+            Irregular myIrregular = new Irregular
+            {
+                Name = " Shanly Anne Gonzales",
+                Program = "BSTM",
+                UnitsEnrolled = "Irregular"
+            };
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n\t****Irregular Student****\n");
+            myIrregular.StudentBasicInfo();
+            myIrregular.EnrolledSemUnits();
+
+            //Wait for user input before closing
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Esquierdo_SimpleInheritance/Esquierdo_SimpleInheritance/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquierdo_SimpleInheritance
+{
+    internal class Student
+    {
+        //Properties for the Student Class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to Display Student Information
+        public void StudentBasicInfo()
+        {
+            Console.WriteLine($"Name: {Name}, \nProgram: {Program}");
+        }
+    }
+
+    class Regular : Student //Derived Class
+    {
+        //Additional property for the Regular class
+        public string Section { get; set; }
+
+        //Method to Display Regular Information
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+    class Irregular : Student //Another derived class
+    {
+        //Additional property for the Irregular Class
+        public string UnitsEnrolled { get; set; }
+
+        //Method to Display Irregular Information
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"UnitsEnrolled: {UnitsEnrolled}");
+        }
+    }
+}
+
